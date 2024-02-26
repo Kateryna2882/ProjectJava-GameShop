d29d2214db9585712472cd6d4dcb30c36b7ed74d
Kateryna2882 <govoryhina82@gmail.com>
2024-02-26 17:08:11 +0200

    Add fields  and methods

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,46 @@
 package org.example;
 
+import org.example.model.User;
+import org.example.service.UserService;
+import org.example.util.DatabaseUtil;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
 public class Main {
+    public static void main(String[] args) {
+        Connection connection = null;
+        try {
+            connection = DatabaseUtil.getConnection();
+            UserService userService = new UserService(connection);
+
+            User user = userService.getUserById(1);
+            if (user != null) {
+                System.out.println("User found by ID:");
+                System.out.println(user);
+            } else {
+                System.out.println("User not found by ID.");
+            }
+
+            List<User> allUsers = userService.getAllUsers();
+            if (!allUsers.isEmpty()) {
+                System.out.println("All users:");
+                for (User u : allUsers) {
+                    System.out.println(u);
+                }
+            } else {
+                System.out.println("No users found.");
+            }
+
+            userService.deleteUser(2);
+            System.out.println("User deleted successfully.");
+        } catch (
+                SQLException e) {
+            System.out.println("Failed to retrieve user by ID: " + e.getMessage());
+        } finally {
+            DatabaseUtil.closeConnection(connection);
+        }
+    }
+
 }
--- src/main/java/org/example/util/DatabaseUtil.java
+++ src/main/java/org/example/util/DatabaseUtil.java
@@ -1,4 +1,32 @@
 package org.example.util;
 
-public class DatabaseUtil {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseUtil { private static final String URL = "jdbc:postgresql://localhost:5432/postgres";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "qweqwepoipoi2321";
+
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Connected to the database");
+        } catch (SQLException e) {
+            System.out.println("Failed to connect to the database: " + e.getMessage());
+        }
+        return connection;
+    }
+
+    public static void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+                System.out.println("Connection to the database closed");
+            } catch (SQLException e) {
+                System.out.println("Failed to close connection: " + e.getMessage());
+            }
+        }
+    }
 }
